234def8d355d15d2626dd697c4374a2770966814
soviedo1983 <141676216+soviedo1983@users.noreply.github.com>
2023-08-08 15:58:05 -0300

    Initial commit

---
+++ src/ej2desarrollo/Ej2desarrollo.java
@@ -0,0 +1,13 @@
+package ej2desarrollo;
+
+import javax.swing.JOptionPane;
+
+public class Ej2desarrollo {
+
+    public static void main(String[] args) {
+        
+         String nombre2="";
+        String nombre = JOptionPane.showInputDialog("Hola como te llamas ?",nombre2);
+    }
+    
+}
