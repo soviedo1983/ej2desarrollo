f5cc71c063c0cd5946a55c69cbd9efd478836d90
HernanBergero <hernanbergero@meyerbergero.com.ar>
2023-08-08 16:23:25 -0300

    Cambio
    Cambio de frase

--- src/ej2desarrollo/Ej2desarrollo.java
+++ src/ej2desarrollo/Ej2desarrollo.java
@@ -7,7 +7,7 @@ public class Ej2desarrollo {
     public static void main(String[] args) {
         
          String nombre2="";
-        String nombre = JOptionPane.showInputDialog("Hola como te llamas ?",nombre2);
+        String nombre = JOptionPane.showInputDialog("Saludos",nombre2);
     }
     
 }
